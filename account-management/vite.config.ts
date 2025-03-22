883bd434a3279899616a5b77165ddb3d3dc821af
pavel.romanov <pavel.romanov@demlabs.net>
2025-03-22 11:53:48 +0100

    fixed versel

--- account-management/vite.config.ts
+++ account-management/vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url'; 
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), 
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   base: './',
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
   },
+  assetsInclude: ['**/*.html'],
 });
